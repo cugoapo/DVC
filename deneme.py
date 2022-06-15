5121f335465bf0ac85931bae08cfb9cfe2eead4c
Alper Hatipoğlu <alperhatipoglu@tabfoods.com>
2022-06-15 13:16:38 +0300

    deneme2

--- deneme.py
+++ deneme.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+x = [10,20,30]
+y = [40,50,60]
+
+df = pd.DataFrame(zip(x,y), columns=['A', 'B'])
+
+sns.scatterplot(x='A', y = 'B', data=df)
+plt.show()
+
